ea77cce180ec9610004c0b8b47cd4ffcf7521d8f
riverbook708 <riverbook708@gmail.com>
2021-06-26 16:54:44 +0900

    add files index.html

--- src/js/App.js
+++ src/js/App.js
@@ -6,7 +6,7 @@ export default function App() {
 
   return (
     <>
-      <h1>I am App Component!!!</h1>
+      <h1>You are App Component!!!</h1>
       <button onClick={() => {
         electron.notificationApi.sendNotification('My custom notification!');
       }}>Notify</button>
