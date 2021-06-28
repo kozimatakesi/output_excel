13a51902614acf12483ef46c4516794ddab21d16
riverbook708 <riverbook708@gmail.com>
2021-06-28 22:41:54 +0900

    before excel output button

--- src/js/App.js
+++ src/js/App.js
@@ -8,7 +8,6 @@ export default function App() {
   return (
     <ChakraProvider>
       <Container>
-        <Text>You are App Component?</Text>
         <SearchDir />
       </Container>
     </ChakraProvider>
--- src/js/SearchDir.js
+++ src/js/SearchDir.js
@@ -12,32 +12,41 @@ const SearchDir = () => {
     setDirPath(inputValue);
   };
 
+  // ドラッグしたディレクトリのパスを入力欄にいれる
   const onDrop = useCallback((acceptedFiles) => {
-    console.log(acceptedFiles);
-    setDirPath(acceptedFiles[0].path);
+    const firstElement = acceptedFiles[0].path.split('/');
+    const lastElement = acceptedFiles[acceptedFiles.length - 1].path.split('/');
+    const array = [];
+    for (let i = 0; i < firstElement.length; i++) {
+      if (firstElement[i] === lastElement[i]) {
+        array.push(firstElement[i]);
+      }
+    }
+    const dragPath = array.join('/');
+    setDirPath(dragPath);
   });
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, isDragActive } = useDropzone({ onDrop });
 
   return (
     <Container>
       <Box {...getRootProps()}>
+        {
+        isDragActive
+          ? <Text>フォルダがドラッグされています</Text>
+          : <Text>フォルダをドラッグしてください</Text>
+      }
         <Input
           value={dirPath}
           onChange={(e) => {
             handleInputChangedDirPath(e);
           }}
         />
-        {
-        isDragActive
-          ? <Text>フォルダがドラッグされています</Text>
-          : <Text>フォルダをドラッグしてください</Text>
-      }
 
       </Box>
       {
         !dirPath.includes('.') && dirPath !== ''
-          ? <Button>EXCEL出力</Button> : ''
+          ? <Button>EXCEL出力</Button> : <Text>ディレクトリが指定されていません</Text>
       }
     </Container>
   );
